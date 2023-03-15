29872e051a0f62f208b26b4e0ef2b239f29becfd
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-15 17:28:53 +0200

    ciclo for fatto

--- js/main.js
+++ js/main.js
@@ -6,7 +6,44 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        todolist : [
+            {
+                testo : 'fare la spesa',
+                done : false
+            },
+            {
+                testo : 'fare la doccia',
+                done : false
+            },
+            {
+                testo : 'comprare sigarette',
+                done : false
+            },
+            {
+                testo : 'studiare',
+                done : false
+            },
+            {
+                testo : 'andare a fare un giro in barca',
+                done : false
+            },
+            {
+                testo : 'cucinare qualcosa',
+                done : false
+            },
+            {
+                testo : 'andare al cinema',
+                done : false
+            },
+            {
+                testo : 'mangiarsi nu bell spaghetto alle vongole',
+                done : false
+            },
+            {
+                testo : 'andare fuori a cena',
+                done : false
+            }
+        ]
       }
     }
   }).mount('#app')
