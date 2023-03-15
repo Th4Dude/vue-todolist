1c6ae64207d6f3a5b25d9e1510f3d1f92ecbc6e0
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-15 18:48:35 +0200

    aggiunto tasto aggiungi

--- js/main.js
+++ js/main.js
@@ -1,56 +1,66 @@
+
 'use strict'
 
 
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        todolist : [
-            {
-                testo : 'fare la spesa',
-                done : false
-            },
-            {
-                testo : 'fare la doccia',
-                done : false
-            },
-            {
-                testo : 'comprare sigarette',
-                done : true
-            },
-            {
-                testo : 'studiare',
-                done : false
-            },
-            {
-                testo : 'andare a fare un giro in barca',
-                done : false
-            },
-            {
-                testo : 'cucinare qualcosa',
-                done : true
-            },
-            {
-                testo : 'andare al cinema',
-                done : false
-            },
-            {
-                testo : 'mangiarsi nu bell spaghetto alle vongole',
-                done : false
-            },
-            {
-                testo : 'andare fuori a cena',
-                done : true
-            }
-        ]
-      }
+        return {
+
+            addtodolist: '',
+
+            todolist: [
+                {
+                    testo: 'fare la spesa',
+                    done: false
+                },
+                {
+                    testo: 'fare la doccia',
+                    done: false
+                },
+                {
+                    testo: 'comprare sigarette',
+                    done: true
+                },
+                {
+                    testo: 'studiare',
+                    done: false
+                },
+                {
+                    testo: 'andare a fare un giro in barca',
+                    done: false
+                },
+                {
+                    testo: 'cucinare qualcosa',
+                    done: true
+                },
+                {
+                    testo: 'andare al cinema',
+                    done: false
+                },
+                {
+                    testo: 'mangiarsi nu bell spaghetto alle vongole',
+                    done: false
+                },
+                {
+                    testo: 'andare fuori a cena',
+                    done: true
+                }
+            ]
+        }
     },
 
-    methods :{
-        rimuovere(index){
-            this.todolist.splice(index,1);
+    methods: {
+        rimuovere(index) {
+            this.todolist.splice(index, 1);
+        },
+        aggiungere() {
+            console.log(this.aggiungere)
+            
         }
 
+
+
     }
-  }).mount('#app')
+}).mount('#app')
